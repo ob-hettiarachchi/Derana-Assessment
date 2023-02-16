b20f40d43918a2580a5c38a1e8e28a58dc5f02ab
OB Hettiarachchi <osanda.buwaneka@gmail.com>
2023-02-16 10:33:54 +0530

    Create page templates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+//import libraries
+import React from "react";
+import {BrowserRouter as Router} from "react-router-dom";
+
+//import components
+import Navigation from "./components/navigation";
+import Footer from "./components/footer";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          <p>Hello</p>
-      </header>
-    </div>
+      <Router>
+          <div>
+
+              <Navigation/>
+              <Footer/>
+
+          </div>
+      </Router>
   );
 }
 export default App;
