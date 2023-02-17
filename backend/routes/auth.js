6c25c084505bcea24a99a077f12019fc919aa226
sathninduk <77549721+ob-hettiarachchi@users.noreply.github.com>
2023-02-17 19:42:19 +0530

    Backend - Backend validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+// config
+const Keys = require("../config/Keys");
+
+// validation
+const validateLoginInput = require("../validation/login");
+
 router.route('/login').post((req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+
+    const {errors, isValid} = validateLoginInput(req.body);
+
+    // Check validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
 
     console.log("test");
 
