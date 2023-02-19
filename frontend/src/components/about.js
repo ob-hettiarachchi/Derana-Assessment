52137394a71b4cb6954a32130be5234978874f08
OB Hettiarachchi <osanda.buwaneka@gmail.com>
2023-02-19 15:57:17 +0530

    Update about.js

--- frontend/src/components/about.js
+++ frontend/src/components/about.js
@@ -9,106 +9,185 @@ export default function About() {
             </Helmet>
             <div className={"form-container con-mid"} style={{marginTop: "56px"}}>
 
-                <div style={{width:"80%", height:"60hw", backgroundColor:"ghostwhite", padding:"60px"}}>
+                <div style={{width: "60%", maxHeight: "80%", backgroundColor: "ghostwhite", paddingTop: "10px", paddingBottom:"10px", paddingLeft:"80px", paddingRight: "80px", margin:"50px"}}>
+
+                    <img src="../derana_logo.png" alt={"derana logo"} style={{width:"10%"}}/>
+                    <br/>
 
                     <h1>About us</h1>
 
-                    <p>
-                        In the highly competitive news broadcasting space, Derana has built probably the most unbiased
-                        and
-                        comprehensive news property on offer "Ada Derana" - with a news team of 100 people geared to
-                        deliver
-                        the most accurate and timely information. Ada Derana To be the number one News brand among Sri
-                        Lankans
-                        in terms of loyalty by reaching them through every possible opportunity in late 2007, Ada Derana
-                        entered into a partnership with Dialog Telekom, the largest mobile phone operator in Sri Lanka,
-                        to
-                        send breaking news alerts to all Dialog mobile subscribers as Short Text Messages (SMS).
-                        Currently,
-                        instant news alerts can be obtained through all the leading operators.
-                    </p>
+                    <b>
 
-                    <p>
-                        Today, Ada Derana is characterised by its availability across delivery channels such as
-                        Television,
-                        Radio, Web, SMS alerts, IVR, Video Alerts, and social media such as Facebook, Twitter and
-                        YouTube.
-                    </p>
+                        <p>
+                            In the highly competitive news broadcasting space, Derana has built probably the most
+                            unbiased
+                            and
+                            comprehensive news property on offer "Ada Derana" - with a news team of 100 people geared to
+                            deliver
+                            the most accurate and timely information. Ada Derana To be the number one News brand among
+                            Sri
+                            Lankans
+                            in terms of loyalty by reaching them through every possible opportunity in late 2007, Ada
+                            Derana
+                            entered into a partnership with Dialog Telekom, the largest mobile phone operator in Sri
+                            Lanka,
+                            to
+                            send breaking news alerts to all Dialog mobile subscribers as Short Text Messages (SMS).
+                            Currently,
+                            instant news alerts can be obtained through all the leading operators.
+                        </p>
 
-                    <p>
-                        In most of these forms, Ada Derana is available on a multilingual platform providing greater
-                        accessibility to consumers both in Sri Lanka and across the world. The style of the news
-                        property
-                        has been customized to suit each of the delivery media, taking note of the nature of
-                        consumption.
-                    </p>
+                        <p>
+                            Today, Ada Derana is characterised by its availability across delivery channels such as
+                            Television,
+                            Radio, Web, SMS alerts, IVR, Video Alerts, and social media such as Facebook, Twitter and
+                            YouTube.
+                        </p>
+
+                        <p>
+                            In most of these forms, Ada Derana is available on a multilingual platform providing greater
+                            accessibility to consumers both in Sri Lanka and across the world. The style of the news
+                            property
+                            has been customized to suit each of the delivery media, taking note of the nature of
+                            consumption.
+                        </p>
+                    </b>
 
                     <h4><i><a href={"https://www.adaderana.lk/"} target={"_blank"}> www.Adaderana.lk </a></i></h4>
 
-                    <p style={{textAlign:"left"}}>
-                        The showpiece of the Ada Derana news property is the www.adaderana.lk web portal which is ranked
-                        the
-                        Number One news site used by Sri Lankans; analysis shows that surfers spend more time on the
-                        site
-                        than they do on that of the nearest competitor. Available in Sinhala, Tamil and English, the
-                        site
-                        facilitates two way viewer generation by offering consumers an interactive platform to comment
-                        on the
-                        latest stories and interact with the Ada Derana community, and by allowing viewers to file in
-                        their
-                        own news reports through Newsdesk - My Report.
-                    </p>
+                    <br/>
 
-                    <p>
-                        Ada Derana, the premium news property, walked away with the SLIM Brand Excellence silver award
-                        for
-                        the Most Innovative Brand of the Year at the awards ceremony held recently. The award was due
-                        recognition
-                        for Ada Derana's drive to become a leading news provider by strategically innovating its
-                        distribution
-                        and delivery. Sri Lanka Institute of Marketing (SLIM) is the nation body of marketing in Sri
-                        Lanka.
-                    </p>
+                    <center>
 
-                    <p>
-                        Ada Derana has been recognized by ICTA as the Best e-Content Application among e-News providers
-                        in
-                        Sri Lanka. www.adaderana.lk was recognized for its tri-lingual presentation of news to readers
-                        in
-                        Sri Lanka and overseas. The award was presented at the e-Swabimani awards 2010 organised by ICTA
-                        at
-                        the Cinnamon Grand Hotel on 9 November 2010. The Information and Communication Technology Agency
-                        (ICTA) of Sri Lanka is the single apex body involved in ICT policy and direction for the nation.
-                        Wholly owned by the Government of Sri Lanka, ICTA is the implementing organization of the e-Sri
-                        Lanka
-                        Initiative. Major donors including the World Bank will be funding a number of the Agency's
-                        initiatives.
-                    </p>
+                        <div style={{width: "90%", backgroundColor: "whitesmoke", padding:"20px"}}>
 
-                    <p>
-                        www.adaderana.lk won the silver award at the "The most Popular Web Competition 2010" organized
-                        by the
-                        Sri lanka Domen registry. This is the only award ceremony in Sri Lanka dedicated for web sites.
-                    </p>
+                            <p style={{textAlign: "left"}}>
+                                The showpiece of the Ada Derana news property is the www.adaderana.lk web portal which
+                                is
+                                ranked
+                                the
+                                Number One news site used by Sri Lankans; analysis shows that surfers spend more time on
+                                the
+                                site
+                                than they do on that of the nearest competitor. Available in Sinhala, Tamil and English,
+                                the
+                                site
+                                facilitates two way viewer generation by offering consumers an interactive platform to
+                                comment
+                                on the
+                                latest stories and interact with the Ada Derana community, and by allowing viewers to
+                                file
+                                in
+                                their
+                                own news reports through Newsdesk - My Report.
+                            </p>
 
-                    <p>
-                        Ada Derana won the Manthan Award South Asia 2010 in the e-News & Media category. The Manthan
-                        Award
-                        given by the Department of Information Technology of India recognizes and rewards the best
-                        e-content
-                        from eight countries in the South Asian region.Ada Derana has the distinction of being the only
-                        Sri
-                        Lankan website among four finalists in the e-News & Media category this year; the other three
-                        finalists
-                        represent India, Pakistan and Bangladesh.
-                    </p>
-
-                    <p>
-                        Ada Derana was awarded the Global Understanding for Sustainable Development Award for 2016 for
-                        its
-                        successful implementation and promotion of the most comprehensive 24 x 7 news portal in Sri
-                        Lanka.
-                    </p>
+                            <ul>
+                                <li style={{textAlign: "left"}}>
+                                    Ada Derana, the premium news property, walked away with the SLIM Brand Excellence
+                                    silver
+                                    award
+                                    for
+                                    the Most Innovative Brand of the Year at the awards ceremony held recently. The
+                                    award
+                                    was
+                                    due
+                                    recognition
+                                    for Ada Derana's drive to become a leading news provider by strategically innovating
+                                    its
+                                    distribution
+                                    and delivery. Sri Lanka Institute of Marketing (SLIM) is the nation body of
+                                    marketing in
+                                    Sri
+                                    Lanka.
+                                </li>
+                                <li style={{textAlign: "left"}}>
+                                    Ada Derana has been recognized by ICTA as the Best e-Content Application among
+                                    e-News
+                                    providers in
+                                    Sri Lanka. www.adaderana.lk was recognized for its tri-lingual presentation of news
+                                    to
+                                    readers in
+                                    Sri Lanka and overseas. The award was presented at the e-Swabimani awards 2010
+                                    organised
+                                    by
+                                    ICTA at
+                                    the Cinnamon Grand Hotel on 9 November 2010. The Information and Communication
+                                    Technology
+                                    Agency
+                                    (ICTA) of Sri Lanka is the single apex body involved in ICT policy and direction for
+                                    the
+                                    nation.
+                                    Wholly owned by the Government of Sri Lanka, ICTA is the implementing organization
+                                    of
+                                    the
+                                    e-Sri Lanka
+                                    Initiative. Major donors including the World Bank will be funding a number of the
+                                    Agency's
+                                    initiatives.
+                                </li>
+                                <li style={{textAlign: "left"}}>
+                                    Ada Derana has been recognized by ICTA as the Best e-Content Application among
+                                    e-News
+                                    providers in
+                                    Sri Lanka. www.adaderana.lk was recognized for its tri-lingual presentation of news
+                                    to
+                                    readers in
+                                    Sri Lanka and overseas. The award was presented at the e-Swabimani awards 2010
+                                    organised
+                                    by
+                                    ICTA at
+                                    the Cinnamon Grand Hotel on 9 November 2010. The Information and Communication
+                                    Technology
+                                    Agency
+                                    (ICTA) of Sri Lanka is the single apex body involved in ICT policy and direction for
+                                    the
+                                    nation.
+                                    Wholly owned by the Government of Sri Lanka, ICTA is the implementing organization
+                                    of
+                                    the
+                                    e-Sri Lanka
+                                    Initiative. Major donors including the World Bank will be funding a number of the
+                                    Agency's
+                                    initiatives.
+                                </li>
+                                <li style={{textAlign: "left"}}>
+                                    www.adaderana.lk won the silver award at the "The most Popular Web Competition 2010"
+                                    organized by the
+                                    Sri lanka Domen registry. This is the only award ceremony in Sri Lanka dedicated for
+                                    web
+                                    sites.
+                                </li>
+                                <li style={{textAlign: "left"}}>
+                                    Ada Derana won the Manthan Award South Asia 2010 in the e-News & Media category. The
+                                    Manthan
+                                    Award
+                                    given by the Department of Information Technology of India recognizes and rewards
+                                    the
+                                    best
+                                    e-content
+                                    from eight countries in the South Asian region.Ada Derana has the distinction of
+                                    being
+                                    the
+                                    only Sri
+                                    Lankan website among four finalists in the e-News & Media category this year; the
+                                    other
+                                    three finalist
+                                    represent India, Pakistan and Bangladesh.
+                                </li>
+                                <li style={{textAlign: "left"}}>
+                                    Ada Derana was awarded the Global Understanding for Sustainable Development Award
+                                    for
+                                    2016
+                                    for its
+                                    successful implementation and promotion of the most comprehensive 24 x 7 news portal
+                                    in
+                                    Sri
+                                    Lanka.
+                                </li>
+                            </ul>
+                        </div>
+                    </center>
 
                 </div>
 
