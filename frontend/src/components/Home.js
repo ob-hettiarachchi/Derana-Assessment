fa510439f8f076b84d96afc138d146d34f10dcca
sathninduk <77549721+ob-hettiarachchi@users.noreply.github.com>
2023-02-19 11:27:13 +0530

    carousel bug - patch

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,40 +7,41 @@ export default function Home() {
             <Helmet>
                 <title>Ada Derana | Home</title>
             </Helmet>
-            <div className={"form-container con-mid"} style={{marginTop: "56px"}}>
+            <div className={"form-container con-mid slideshow-custom"}>
 
-                <div className={"carousel slide"} data-ride="carousel">
-                    <div className={"carousel-inner"}>
-                        <div className={"carousel-item active"}>
-                            <img className={"d-block w-100"} src="https://cdn.pixabay.com/photo/2016/09/01/19/53/pocket-watch-1637396_960_720.jpg" alt="First slide"/>
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>text1</h5>
-                                <p>text text text text text text text text text.</p>
-                            </div>
+
+
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active"/>
+                        <li data-target="#carouselExampleIndicators" data-slide-to={1}/>
+                        <li data-target="#carouselExampleIndicators" data-slide-to={2}/>
+                    </ol>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img className="d-block" style={{width: "100vw"}}
+                                 src="https://cdn.pixabay.com/photo/2016/09/01/19/53/pocket-watch-1637396_960_720.jpg"
+                                 alt="First slide"/>
                         </div>
-                        <div className={"carousel-item"}>
-                            <img className={"d-block w-100"} src="https://cdn.pixabay.com/photo/2017/04/11/15/55/animals-2222007_960_720.jpg" alt="Second slide"/>
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>text2</h5>
-                                <p>text text text text text text text text text.</p>
-                            </div>
+                        <div className="carousel-item">
+                            <img className="d-block" style={{width: "100vw"}}
+                                 src="https://cdn.pixabay.com/photo/2017/04/11/15/55/animals-2222007_960_720.jpg"
+                                 alt="Second slide"/>
                         </div>
-                        <div className={"carousel-item"}>
-                            <img className={"d-block w-100"} src="https://cdn.pixabay.com/photo/2015/12/06/09/15/maple-1079235_960_720.jpg" alt="Third slide"/>
-                            <div className="carousel-caption d-none d-md-block">
-                                <h5>text3</h5>
-                                <p>text text text text text text text text text.</p>
-                            </div>
+                        <div className="carousel-item">
+                            <img className="d-block" style={{width: "100vw"}}
+                                 src="https://cdn.pixabay.com/photo/2015/12/06/09/15/maple-1079235_960_720.jpg"
+                                 alt="Third slide"/>
                         </div>
                     </div>
-                    <a className="carousel-control-prev" href="/" role="button"
+                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button"
                        data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="carousel-control-prev-icon" aria-hidden="true"/>
                         <span className="sr-only">Previous</span>
                     </a>
-                    <a className="carousel-control-next" href="/" role="button"
+                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button"
                        data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="carousel-control-next-icon" aria-hidden="true"/>
                         <span className="sr-only">Next</span>
                     </a>
                 </div>
@@ -52,7 +53,7 @@ export default function Home() {
 
                 <div className="container">
                     <div className="row">
-                        <div className="col-sm" style={{backgroundColor:"whitesmoke", margin:"10px"}}>
+                        <div className="col-sm" style={{backgroundColor: "whitesmoke", margin: "10px"}}>
                             <br/>
                             <br/>
                             <a href={"https://www.youtube.com/channel/UCCK3OZi788Ok44K97WAhLKQ"}>
@@ -63,7 +64,7 @@ export default function Home() {
                             <br/>
                             <br/>
                         </div>
-                        <div className="col-sm" style={{backgroundColor:"whitesmoke", margin:"10px"}}>
+                        <div className="col-sm" style={{backgroundColor: "whitesmoke", margin: "10px"}}>
                             <br/>
                             <br/>
                             <a href={"https://www.adaderana.lk/"}>
@@ -74,7 +75,7 @@ export default function Home() {
                             <br/>
                             <br/>
                         </div>
-                        <div className="col-sm" style={{backgroundColor:"whitesmoke", margin:"10px"}}>
+                        <div className="col-sm" style={{backgroundColor: "whitesmoke", margin: "10px"}}>
                             <br/>
                             <br/>
                             <a href={"/news/1"}>
